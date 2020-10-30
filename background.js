85043e9d4fa9150084a4dfbe3578cd2cd9887220
Haejoon Kim <onsealeatang@gmail.com>
2020-10-31 03:44:36 +0900

    handling exceptional sites whose origin is not "medium.com"

--- background.js
+++ background.js
@@ -1,14 +1,26 @@
-chrome.webNavigation.onDOMContentLoaded.addListener(function({tabId, url}){
-    if (!!url && url.match(/^https\:\/\/(.+\.)?medium\.com\/.+$/)){
-        chrome.tabs.executeScript(null, {
-            code: 'document.all[0].innerText',
-            allFrames: false,
-        }, function(results){
-            var content = results[0];
-            if (content.match(/Read the rest of this story with a free account/)){
-                chrome.windows.create({url: url, incognito: true});
-                chrome.tabs.remove(tabId);
-            }
-        });
-    }
+var loginRequired = "Read the rest of this story with a free account";
+var membershipRequired = "Become a member to get unlimited access";
+
+chrome.webNavigation.onCompleted.addListener(function({tabId, url, frameId}){
+    if (frameId !== 0) return;
+    if (url.match(/^chrome\:\/\//)) return;
+
+    chrome.tabs.executeScript(tabId, {
+        code: `document.head.querySelector("meta[property='og:site_name']").getAttribute("content")`
+    }, function(results){
+        if (!results) return;
+        var siteName = results[0];
+        if (siteName === "Medium"){
+            chrome.tabs.executeScript(tabId, {
+                code: 'document.all[0].innerText',
+            }, function(results){
+                if (!results) return;
+                var content = results[0];
+                if (content.match(`${loginRequired}|${membershipRequired}`)){
+                    chrome.windows.create({url, incognito: true});
+                    chrome.tabs.remove(tabId);
+                }
+            });
+        }
+    });
 });
